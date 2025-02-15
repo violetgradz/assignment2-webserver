4f691b49af646cc111326d95ced3819aefc4e0dd
Beatriz D <126616469+dat-angel@users.noreply.github.com>
2025-02-15 15:34:42 -0800

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -1,51 +1,41 @@
-# import socket module
 from socket import *
-# In order to terminate the program
 import sys
 
-
 def webServer(port=13331):
-  serverSocket = socket(AF_INET, SOCK_STREAM)
-  
-  #Prepare a server socket
-  serverSocket.bind(("", port))
-  #Fill in start
-  serverSocket.listen(1)
-  #Fill in end
-
-  while True:
-    #Establish the connection
+    serverSocket = socket(AF_INET, SOCK_STREAM)
     
-    print('Ready to serve...')
-
-    #Fill in start -are you accepting connections?
-    connectionSocket, addr = 
+    serverSocket.bind(("", port))
+    #Fill in start
+    serverSocket.listen(1)  # only need 1 connection at a time
     #Fill in end
     
-    try:
-      #Fill in start
-      message = connectionSocket.recv(1024).decode() #gets https request
-      #Fill in end 
-      
-      #get filename from GET request
-      filename = message.split()[1]
-      
-      #Fill in start
-      f = open(filename[1:], "r")
-      #fill in end
-      
-
-      #Fill in start
-      #make response headers
-      outputdata = "HTTP/1.1 200 OK\r\n"
-      outputdata += "Content-Type: text/html; charset=UTF-8\r\n"  
-      outputdata += "Server: MyServer\r\n"   #changed server name
-      outputdata += "Connection: close\r\n\r\n"  #need empty line after headers
+    while True:
+        print('Ready to serve...')        
+        #Fill in start
+        connectionSocket, addr = serverSocket.accept()  #accept client connection
+        #Fill in end
+        
+        try:
+            #Fill in start 
+            message = connectionSocket.recv(1024).decode()  #get http request
+            #Fill in end
             
-      #get file contents
-      filedata = ""
-      for line in f:  #read file line by line
-        filedata += line
+            filename = message.split()[1]  #get filename from GET request            
+            #Fill in start
+            f = open(filename[1:], "r")  #remove first / from filename
+            #Fill in end
+            
+            #Fill in start
+            #make response headers
+            outputdata = "HTTP/1.1 200 OK\r\n"
+            outputdata += "Content-Type: text/html; charset=UTF-8\r\n"  
+            outputdata += "Server: MyServer\r\n"   #changed server name
+            outputdata += "Connection: close\r\n\r\n"  #need empty line after headers
+            
+            #get file contents
+            filedata = ""
+            for line in f:  #read file line by line
+                filedata += line
             
             #send everything at once
             connectionSocket.send(outputdata.encode() + filedata.encode())
